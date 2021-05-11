96e3208d2274a55198dd439a9d12b598624f0e79
Alessandro Liparoti <alessandro.liparoti@booking.com>
2021-05-12 00:39:54 +0200

    comment out old main

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,55 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:nutmeg/matches_available.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    home: new MyApp(),
-  ));
-}
+// void main() {
+//   runApp(new MaterialApp(
+//     home: new MyApp(),
+//   ));
+// }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-}
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => new _MyAppState();
+// }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-
-    // initialization code
-    Firebase.initializeApp();
-
-    new Future.delayed(
-        const Duration(seconds: 1),
-            () => Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ListPage()),
-        ));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        child: new Column(children: <Widget>[
-          Divider(
-            height: 240.0,
-            color: Colors.white,
-          ),
-          new Image.asset(
-            'assets/football.jpg',
-            fit: BoxFit.cover,
-            repeat: ImageRepeat.noRepeat,
-            width: 170.0,
-          ),
-          Divider(
-            height: 105.2,
-            color: Colors.white,
-          ),
-        ]),
-      ),
-    );
-  }
-}
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   void initState() {
+//     super.initState();
+//
+//     // initialization code
+//     Firebase.initializeApp();
+//
+//     new Future.delayed(
+//         const Duration(seconds: 1),
+//             () => Navigator.push(
+//           context,
+//           MaterialPageRoute(builder: (context) => ListPage()),
+//         ));
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Container(
+//         child: new Column(children: <Widget>[
+//           Divider(
+//             height: 240.0,
+//             color: Colors.white,
+//           ),
+//           new Image.asset(
+//             'assets/football.jpg',
+//             fit: BoxFit.cover,
+//             repeat: ImageRepeat.noRepeat,
+//             width: 170.0,
+//           ),
+//           Divider(
+//             height: 105.2,
+//             color: Colors.white,
+//           ),
+//         ]),
+//       ),
+//     );
+//   }
+// }
