cfd3a625d2411f4cb6951941af70ab0d8c803a2f
alessandrol <alessandrol@spotify.com>
2021-09-04 19:56:24 +0200

    some changes for web (still wip)

--- lib/screens/Launch.dart
+++ lib/screens/Launch.dart
@@ -9,14 +9,17 @@ import 'package:package_info_plus/package_info_plus.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart' show kDebugMode;
+import 'package:flutter/foundation.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized(); //imp line need to be added first
 
-  FlutterError.onError = (FlutterErrorDetails details) async {
-    print("*** CAUGHT FROM FRAMEWORK ***");
-    await FirebaseCrashlytics.instance.recordFlutterError(details);
-  };
+  if (!kIsWeb) {
+    FlutterError.onError = (FlutterErrorDetails details) async {
+      print("*** CAUGHT FROM FRAMEWORK ***");
+      await FirebaseCrashlytics.instance.recordFlutterError(details);
+    };
+  }
 
   runZonedGuarded(() {
     runApp(MultiProvider(
@@ -36,18 +39,23 @@ void main() {
     ));
   }, (Object error, StackTrace stackTrace) {
     print("**** ZONED EXCEPTION ****");
-    FirebaseCrashlytics.instance.recordError(error, stackTrace);
+    if (!kIsWeb) {
+      FirebaseCrashlytics.instance.recordError(error, stackTrace);
+    }
   });
 }
 
 class LaunchWidget extends StatelessWidget {
   Future<void> loadData(BuildContext context) async {
+    print("app initialization tasks running");
     await Firebase.initializeApp();
 
     if (kDebugMode) {
       // Force disable Crashlytics collection while doing every day development.
       // Temporarily toggle this to true if you want to test crash reporting in your app.
-      await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+      if (!kIsWeb) {
+        await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+      }
     } else {
       // Handle Crashlytics enabled status when not in Debug,
       // e.g. allow your users to opt-in to crash reporting.
@@ -78,8 +86,10 @@ class LaunchWidget extends StatelessWidget {
           color: Palette.primary,
         ),
         child: FutureBuilder<void>(
-          future: loadData(context)
-              .catchError((err) => defaultErrorMessage(err, context)),
+          future: loadData(context).catchError((err, stacktrace) {
+            print(err);
+            defaultErrorMessage(err, context);
+          }),
           builder: (context, snapshot) => (snapshot.hasError)
               ? Text(snapshot.error.toString(),
                   style: TextPalette.linkStyleInverted)
