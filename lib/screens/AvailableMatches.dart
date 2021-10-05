a25f6c37d87dc2f85d517a3f2e4fd0a02766392b
Alessandro Liparoti <alessandrol@spotify.com>
2021-10-05 23:33:58 +0200

    fix exception handling in login

--- lib/screens/AvailableMatches.dart
+++ lib/screens/AvailableMatches.dart
@@ -64,7 +64,7 @@ class RoundedTopBar extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Find football gamess in",
+              Text("Find football games in",
                   style: TextPalette.bodyTextInverted),
               Text("Amsterdam", style: TextPalette.h1Inverted),
               SizedBox(height: 24),
--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nutmeg/controller/UserController.dart';
 import 'package:nutmeg/model/ChangeNotifiers.dart';
+import 'package:nutmeg/utils/InfoModals.dart';
 import 'package:nutmeg/utils/UiUtils.dart';
 import 'package:nutmeg/widgets/Containers.dart';
 import 'package:provider/provider.dart';
@@ -76,76 +77,6 @@ class LoginArea extends StatelessWidget {
   }
 }
 
-class GoogleSignInButton extends StatelessWidget {
-  final Function onPressed;
-
-  const GoogleSignInButton({Key key, this.onPressed}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      child: Row(
-        children: [
-          Expanded(
-            child: OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                  side: BorderSide(width: 1.0, color: Palette.lightGrey),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40))),
-              onPressed: () async {
-                context.read<LoginStatusChangeNotifier>().setIsSigningIn(true);
-
-                try {
-                  var communication = await UserController.continueWithGoogle(
-                      context.read<UserState>());
-                  Navigator.pop(context, communication);
-                } on FirebaseAuthException catch (e) {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(content: Text(e.message));
-                      });
-                } on PlatformException catch (e) {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                            content: Text(e.code + " " + e.message));
-                      });
-                } finally {
-                  context
-                      .read<LoginStatusChangeNotifier>()
-                      .setIsSigningIn(false);
-                }
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Image(
-                    image: AssetImage("assets/google_logo.png"),
-                    height: 20.0,
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Text(
-                        'CONTINUE WITH GOOGLE',
-                        style: TextPalette.bodyText,
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
 enum Provider { facebook, google }
 
 class SignInButton extends StatelessWidget {
@@ -195,19 +126,11 @@ class SignInButton extends StatelessWidget {
                 try {
                   var communication = await loginFuture();
                   Navigator.pop(context, communication);
-                } on FirebaseAuthException catch (e) {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(content: Text(e.message));
-                      });
-                } on PlatformException catch (e) {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                            content: Text(e.code + " " + e.message));
-                      });
+                } on Exception catch (e, stack) {
+                  print(e);
+                  print(stack);
+                  GenericInfoModal(title: "Sign-in failed",
+                      body: "Please try again or reach out for support").show(context);
                 } finally {
                   context
                       .read<LoginStatusChangeNotifier>()
@@ -234,63 +157,6 @@ class SignInButton extends StatelessWidget {
                 ],
               ),
             ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class FacebookSignInButton extends StatelessWidget {
-  final Function onPressed;
-
-  const FacebookSignInButton({Key key, this.onPressed}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return OutlinedButton(
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(Colors.white),
-        shape: MaterialStateProperty.all(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(40),
-          ),
-        ),
-      ),
-      onPressed: () async {
-        context.read<LoginStatusChangeNotifier>().setIsSigningIn(true);
-
-        try {
-          var communication = await UserController.continueWithFacebook(
-              context.read<UserState>());
-          Navigator.pop(context, communication);
-        } on FirebaseAuthException catch (e) {
-          showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(content: Text(e.message));
-              });
-        } on PlatformException catch (e) {
-          showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(content: Text(e.code + " " + e.message));
-              });
-        } finally {
-          context.read<LoginStatusChangeNotifier>().setIsSigningIn(false);
-        }
-      },
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Image(
-            image: AssetImage("assets/google_logo.png"),
-            height: 20.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: Text('CONTINUE WITH FACEBOOK', style: TextPalette.bodyText),
           )
         ],
       ),
