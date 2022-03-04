3d41ab0374cf57cb96f5f7f983fccdfc531c8128
Alessandro Liparoti <alessandrol@spotify.com>
2022-03-04 16:17:01 +0100

    fix script and bump

--- create_release_from_tag.sh
+++ create_release_from_tag.sh
@@ -1,8 +1,18 @@
 set -x
 
 TAG=$1
+NOTES=$2
 BUILD=$(echo $TAG | cut -d'+' -f2)
 VERSION=$(echo $TAG | cut -d'v' -f2 | cut -d'+' -f1)
+
+if [ -z "$NOTES" ]
+then
+  echo "pass release notes as second argument"
+  exit -1
+fi
+
+# populate ios release notes
+echo $NOTES > ios/fastlane/metadata/en-GB/release_notes.txt
 
 # promote android build
 (cd android; fastlane run upload_to_play_store track_promote_to:production version_code:$BUILD track:internal)
@@ -11,7 +21,7 @@ VERSION=$(echo $TAG | cut -d'v' -f2 | cut -d'+' -f1)
 (cd ios; fastlane submit_for_review build_number:$BUILD version:$VERSION --env .env.relase)
 
 # create release in github
-gh release create $TAG --notes "General improvements"
+gh release create $TAG --notes $NOTES
 
 # bump minor (we need to do this because of appstore)
 python bump_patch.py
