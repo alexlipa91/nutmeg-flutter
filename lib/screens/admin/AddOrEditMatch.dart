a805c2bd76542371ee60b23a66d3f514f9856aaa
Alessandro Liparoti <alessandrol@spotify.com>
2022-03-24 12:04:35 +0100

    visualize ratings received

--- lib/screens/admin/AddOrEditMatch.dart
+++ lib/screens/admin/AddOrEditMatch.dart
@@ -1,13 +1,11 @@
 import 'package:cool_alert/cool_alert.dart';
-import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:intl/intl.dart';
-import 'package:nutmeg/Exceptions.dart';
+import 'package:nutmeg/api/CloudFunctionsUtils.dart';
 import 'package:nutmeg/controller/MatchesController.dart';
-import 'package:nutmeg/model/Match.dart';
 import 'package:nutmeg/state/UserState.dart';
 import 'package:nutmeg/utils/InfoModals.dart';
 import 'package:nutmeg/utils/UiUtils.dart';
@@ -15,8 +13,6 @@ import 'package:nutmeg/widgets/ButtonsWithLoader.dart';
 import 'package:provider/provider.dart';
 
 import '../../controller/UserController.dart';
-import '../../model/Sport.dart';
-import '../../model/SportCenter.dart';
 import '../../state/LoadOnceState.dart';
 import '../../state/MatchesState.dart';
 
@@ -120,6 +116,8 @@ class AddOrEditMatchFormState extends State<AddOrEditMatchForm> {
   TextEditingController durationController;
   bool testMatch;
 
+  Map<String, dynamic> ratings;
+
   @override
   void initState() {
     var matchesState = context.read<MatchesState>();
@@ -148,6 +146,8 @@ class AddOrEditMatchFormState extends State<AddOrEditMatchForm> {
         text: (match == null) ? "60" : match.duration.inMinutes.toString());
     testMatch = (match == null) ? false : match.isTest;
     super.initState();
+
+    loadState();
   }
 
   Future<void> loadState() async {
@@ -158,6 +158,10 @@ class AddOrEditMatchFormState extends State<AddOrEditMatchForm> {
       await Future.wait(match
           .getGoingUsersByTime()
           .map((e) => UserController.getUserDetails(context, e)));
+
+    if (matchId != null && match.dateTime.isBefore(DateTime.now()))
+      ratings = await CloudFunctionsClient()
+          .callFunction("get_ratings_by_match", {"match_id": matchId});
   }
 
   @override
@@ -185,215 +189,11 @@ class AddOrEditMatchFormState extends State<AddOrEditMatchForm> {
             children: [
               if (matchId != null)
                 SelectableText("Match id: " + matchId, style: TextPalette.h2),
-              SizedBox(height: 20.0),
+              SizedBox(height: 16.0),
               if (matchId != null)
                 Text("Status is: " + status.toString().split(".").last,
                     style: TextPalette.h2),
-              SizedBox(height: 20.0),
-              Text("Date and Time", style: TextPalette.linkStyle),
-              DateTimePicker(
-                type: DateTimePickerType.dateTimeSeparate,
-                dateMask: 'd MMM, yyyy',
-                initialDate: dateTime,
-                initialValue: (dateTime != null) ? dateTime.toString() : null,
-                firstDate: DateTime(2000),
-                lastDate: DateTime(2100),
-                icon: Icon(Icons.event),
-                dateLabelText: 'Date',
-                timeLabelText: "Hour",
-                onChanged: (val) {
-                  dateTime = DateTime.parse(val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              Text("SportCenter", style: TextPalette.linkStyle),
-              DropdownButton<SportCenter>(
-                focusColor: Colors.white,
-                value:
-                    context.read<LoadOnceState>().getSportCenter(sportCenterId),
-                style: TextStyle(color: Colors.white),
-                iconEnabledColor: Colors.black,
-                items: loadOnceState
-                    .getSportCenters()
-                    .map<DropdownMenuItem<SportCenter>>((SportCenter value) {
-                  return DropdownMenuItem<SportCenter>(
-                      value: value,
-                      child: Text(
-                          value.getName() == null ? "null" : value.getName(),
-                          style: TextStyle(color: Colors.black)));
-                }).toList(),
-                onChanged: (SportCenter value) {
-                  setState(() {
-                    sportCenterId = value.placeId;
-                  });
-                },
-              ),
-              SizedBox(height: 20.0),
-              Text("SportCenter location additional info (e.g. hall)",
-                  style: TextPalette.linkStyle),
-              TextFormField(
-                initialValue: sportCenterSubLocation,
-                onChanged: (v) => sportCenterSubLocation = v,
-              ),
-              SizedBox(height: 20.0),
-              Text("Sport", style: TextPalette.linkStyle),
-              DropdownButton<Sport>(
-                focusColor: Colors.white,
-                value: loadOnceState.getSport(sport),
-                //elevation: 5,
-                style: TextStyle(color: Colors.white),
-                iconEnabledColor: Colors.black,
-                items: loadOnceState
-                    .getSports()
-                    .map<DropdownMenuItem<Sport>>((Sport value) {
-                  return DropdownMenuItem<Sport>(
-                      value: value,
-                      child: Text(value.displayTitle,
-                          style: TextPalette.linkStyle));
-                }).toList(),
-                onChanged: (Sport value) {
-                  setState(() {
-                    sport = value.documentId;
-                  });
-                },
-              ),
-              SizedBox(height: 20.0),
-              Text("Price per person (in euro)", style: TextPalette.linkStyle),
-              TextFormField(
-                controller: priceController,
-                decoration: InputDecoration(prefixText: "Euro "),
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d*')),
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Text("Duration (in minutes)", style: TextPalette.linkStyle),
-              TextFormField(
-                controller: durationController,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.allow(RegExp(r'\d+')),
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Text("Max players", style: TextPalette.linkStyle),
-              DropdownButton<int>(
-                focusColor: Colors.white,
-                value: maxPlayers,
-                style: TextStyle(color: Colors.white),
-                iconEnabledColor: Colors.black,
-                items: maxPlayersDropdownItemsSet
-                    .toList()
-                    .map<DropdownMenuItem<int>>((int value) {
-                  return DropdownMenuItem<int>(
-                      value: value,
-                      child:
-                          Text(value.toString(), style: TextPalette.linkStyle));
-                }).toList(),
-                onChanged: (int value) {
-                  setState(() {
-                    maxPlayers = value;
-                  });
-                },
-              ),
-              Row(
-                children: [
-                  Text("Test Match"),
-                  SizedBox(width: 10),
-                  Switch(
-                    value: testMatch,
-                    onChanged: (value) {
-                      setState(() {
-                        testMatch = value;
-                      });
-                    },
-                    activeTrackColor: Colors.red,
-                    activeColor: Colors.red,
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: GenericButtonWithLoader(
-                        (match == null) ? "ADD" : "EDIT",
-                        (BuildContext context) async {
-                      context.read<GenericButtonWithLoaderState>().change(true);
-                      try {
-                        if (match == null) {
-                          var shouldAdd = await CoolAlert.show(
-                            context: context,
-                            type: CoolAlertType.confirm,
-                            text:
-                                "This is going to add a new match. Are you sure?",
-                            onConfirmBtnTap: () => Get.back(result: true),
-                            onCancelBtnTap: () => Get.back(result: false),
-                          );
-                          if (shouldAdd) {
-                            var newMatchId = await MatchesController.addMatch(
-                                Match(
-                                    dateTime,
-                                    sportCenterId,
-                                    sportCenterSubLocation,
-                                    sport,
-                                    maxPlayers,
-                                    (double.parse(priceController.value.text) *
-                                            100)
-                                        .toInt(),
-                                    Duration(
-                                        minutes: int.parse(
-                                            durationController.value.text)),
-                                    testMatch));
-                            await MatchesController.refresh(
-                                context, newMatchId);
-                            await GenericInfoModal(
-                                    title: "Success",
-                                    description:
-                                        "Added match with id:\n" + newMatchId)
-                                .show(context);
-                          }
-                        } else {
-                          match.dateTime = dateTime;
-                          match.sportCenterId = sportCenterId;
-                          match.sportCenterSubLocation = sportCenterSubLocation;
-                          match.sport = sport;
-                          match.maxPlayers = maxPlayers;
-                          match.pricePerPersonInCents =
-                              (double.parse(priceController.value.text) * 100)
-                                  .toInt();
-
-                          var shouldUpdate = await CoolAlert.show(
-                            context: context,
-                            type: CoolAlertType.confirm,
-                            text:
-                                "This is going to update the match with id: \n" +
-                                    match.documentId +
-                                    "\nAre you sure?",
-                            onConfirmBtnTap: () => Get.back(result: true),
-                            onCancelBtnTap: () => Get.back(result: false),
-                          );
-                          if (!shouldUpdate) {
-                            return;
-                          }
-
-                          await MatchesController.editMatch(match);
-                          await GenericInfoModal(
-                                  title: "Success!",
-                                  description: "Match with id " +
-                                      match.documentId +
-                                      " successfully modified")
-                              .show(context);
-                          Get.back();
-                        }
-                      } catch (e, stackTrace) {
-                        ErrorHandlingUtils.handleError(e, stackTrace, context);
-                      }
-                      context
-                          .read<GenericButtonWithLoaderState>()
-                          .change(false);
-                    }, Primary()),
-                  )
-                ],
-              ),
+              SizedBox(height: 16.0),
               if (matchId != null)
                 Row(
                   children: [
@@ -494,7 +294,7 @@ class AddOrEditMatchFormState extends State<AddOrEditMatchForm> {
                   child: Column(
                     children: [
                       Text(
-                        "Users",
+                        "Players",
                         style: TextPalette.h2,
                       ),
                       Column(
@@ -509,6 +309,44 @@ class AddOrEditMatchFormState extends State<AddOrEditMatchForm> {
                                           style: TextPalette.h3);
                                     })
                                   ]))
+                              .toList())
+                    ],
+                  ),
+                ),
+              SizedBox(height: 16),
+              if (ratings != null && ratings.isNotEmpty)
+                Container(
+                  child: Column(
+                    children: [
+                      Text(
+                        "Ratings Received",
+                        style: TextPalette.h2,
+                      ),
+                      Column(
+                          children: ratings.entries
+                              .map((e) => Builder(builder: (context) {
+                                    var ud = context
+                                        .read<UserState>()
+                                        .getUserDetail(e.key);
+                                    if (ud == null) return Container();
+                                    var skips = e.value.where((v) => v == -1);
+                                    var votes = e.value.where((v) => v != -1);
+
+                                    return Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(ud.name ?? "Player",
+                                            style: TextPalette.h3),
+                                        Text(
+                                            votes.length.toString() +
+                                                " votes\t\t" +
+                                                skips.length.toString() +
+                                                " skip",
+                                            style: TextPalette.h3),
+                                      ],
+                                    );
+                                  }))
                               .toList())
                     ],
                   ),
