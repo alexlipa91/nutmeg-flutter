7e7614482eb88b07d2bed05abff6f41ff090ccf9
Alessandro Liparoti <alessandrol@spotify.com>
2022-06-08 17:11:53 +0200

    fix dimension of the scroll dot

--- lib/screens/MatchDetails.dart
+++ lib/screens/MatchDetails.dart
@@ -483,8 +483,8 @@ class SportCenterImageCarouselState extends State<SportCenterImageCarousel> {
                 return GestureDetector(
                   onTap: () => _controller.animateToPage(entry.key),
                   child: Container(
-                    width: 12.0,
-                    height: 12.0,
+                    width: 8.0,
+                    height: 8.0,
                     margin:
                         EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
                     decoration: BoxDecoration(
