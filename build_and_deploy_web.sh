ce6690e589deb9cade41798ac60eb001f4e84a92
Alessandro Liparoti <alessandrolipa@meta.com>
2023-03-27 15:37:55 +0200

    use canvaskit

--- build_and_deploy_web.sh
+++ build_and_deploy_web.sh
@@ -1,5 +1,5 @@
-flutter build web --release --no-sound-null-safety -t lib/screens/Launch.dart
+flutter build web --release --web-renderer=canvaskit --no-sound-null-safety -t lib/screens/Launch.dart
 firebase deploy
 
-flutter build web --release --no-sound-null-safety -t lib/screens/Launch.dart --dart-define="LOCALE=pt"
+flutter build web --release --web-renderer=canvaskit --no-sound-null-safety -t lib/screens/Launch.dart --dart-define="LOCALE=pt"
 firebase hosting:channel:deploy pt
