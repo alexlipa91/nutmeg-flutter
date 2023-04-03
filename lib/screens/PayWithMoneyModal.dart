6089cd19737f63e168d80c824320f175b85c5bd7
Alessandro Liparoti <alessandrolipa@meta.com>
2023-04-03 15:34:39 +0200

    use dynamic links

--- lib/screens/PayWithMoneyModal.dart
+++ lib/screens/PayWithMoneyModal.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:nutmeg/state/MatchesState.dart';
 import 'package:nutmeg/widgets/ButtonsWithLoader.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-import '../controller/MatchesController.dart';
 import '../model/PaymentRecap.dart';
 import '../state/UserState.dart';
 
 import '../utils/UiUtils.dart';
-import 'PaymentDetailsDescription.dart';
 
 class PayWithMoneyButton extends StatelessWidget {
 
@@ -30,67 +27,52 @@ class PayWithMoneyButton extends StatelessWidget {
           context.read<GenericButtonWithLoaderState>().change(true);
 
           var userState = context.read<UserState>();
-          var match = context.read<MatchesState>().getMatch(matchId);
 
           var url = "https://nutmeg-9099c.ew.r.appspot.com/payments/checkout?"
-              "user_id=${userState.currentUserId}&match_id=$matchId";
+              "user_id=${userState.currentUserId}&match_id=$matchId&v=2";
+
+          var uri = Uri.parse(url);
 
           if (kIsWeb)
-            await launchUrl(Uri.parse(url), webOnlyWindowName: "_self");
-          else {
-            bool? success = await Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Payment(matchId: matchId, url: url)),
-            );
-            Navigator.pop(context);
-            if (success != null) {
-              if (success) {
-                PaymentDetailsDescription.communicateSuccessToUser(context, match!);
-                MatchesController.refresh(context, match.documentId);
-              }
-              // else {
-              //   GenericInfoModal(
-              //       title: "Payment Failed!", description: "Please try again")
-              //       .show(context);
-              // }
-            }
-          }
+            await launchUrl(uri, webOnlyWindowName: "_self");
+          else
+            await launchUrl(uri);
         },
         Primary(),
       );
 }
 
-class Payment extends StatelessWidget {
-  final String matchId;
-  final String url;
-
-  const Payment({Key? key, required this.matchId, required this.url})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Palette.primary,
-      body: SafeArea(
-        bottom: false,
-        child: WebViewWidget(
-          controller: WebViewController()
-            ..setJavaScriptMode(JavaScriptMode.unrestricted)
-            ..setBackgroundColor(const Color(0x00000000))
-            ..setNavigationDelegate(
-              NavigationDelegate(
-                onNavigationRequest: (NavigationRequest request) {
-                  if (request.url == "https://web.nutmegapp.com/match/$matchId?payment_outcome=success")
-                    Navigator.pop(context, true);
-                  else if (request.url == "https://web.nutmegapp.com/match/$matchId?payment_outcome=cancel")
-                    Navigator.pop(context, false);
-                  return NavigationDecision.navigate;
-                },
-              ),
-            )
-            ..loadRequest(Uri.parse(url)),
-        ),
-      ),
-    );
-  }
-}
+// class Payment extends StatelessWidget {
+//   final String matchId;
+//   final String url;
+//
+//   const Payment({Key? key, required this.matchId, required this.url})
+//       : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Palette.primary,
+//       body: SafeArea(
+//         bottom: false,
+//         child: WebViewWidget(
+//           controller: WebViewController()
+//             ..setJavaScriptMode(JavaScriptMode.unrestricted)
+//             ..setBackgroundColor(const Color(0x00000000))
+//             ..setNavigationDelegate(
+//               NavigationDelegate(
+//                 onNavigationRequest: (NavigationRequest request) {
+//                   if (request.url == "https://web.nutmegapp.com/match/$matchId?payment_outcome=success")
+//                     Navigator.pop(context, true);
+//                   else if (request.url == "https://web.nutmegapp.com/match/$matchId?payment_outcome=cancel")
+//                     Navigator.pop(context, false);
+//                   return NavigationDecision.navigate;
+//                 },
+//               ),
+//             )
+//             ..loadRequest(Uri.parse(url)),
+//         ),
+//       ),
+//     );
+//   }
+// }
