a4fb14991a5449e57b1df4390221b3006dc89df2
Alessandro Liparoti <alessandroliparoti@live.it>
2023-05-12 19:07:41 +0200

    improve leaderboard

--- lib/screens/Leaderboard.dart
+++ lib/screens/Leaderboard.dart
@@ -47,8 +47,7 @@ class LeaderboardScreenState extends State<LeaderboardScreen> {
     setState(() {
       leaderboard = Leaderboard.fromJson(selectedLeaderboard, l!);
       this.leaderboard!.entries.sort((a, b) {
-        return LeaderboardEntry.compareBy(
-            a, b, sortColumnIndex);
+        return LeaderboardEntry.compareBy(a, b, sortColumnIndex);
       });
     });
     leaderboard!.entries.forEach((e) {
@@ -141,99 +140,109 @@ class LeaderboardScreenState extends State<LeaderboardScreen> {
               SizedBox(height: 16),
               if (leaderboard != null)
                 InfoContainer(
+                    padding: EdgeInsets.all(8),
                     child: Container(
-                  height: MediaQuery.of(context).size.height,
-                  child: DataTable2(
-                      sortColumnIndex: sortColumnIndex,
-                      sortAscending: sortAscending,
-                      dataRowHeight: 36,
-                      columnSpacing: 8,
-                      dividerThickness: 0,
-                      // border: TableBorder.all(color: Colors.grey),
-                      horizontalMargin: 0,
-                      onSelectAll: (val) => print('s'),
-                      columns: [
-                        DataColumn2(
-                          fixedWidth: 30,
-                          label: Text('', style: TextPalette.h3),
-                        ),
-                        DataColumn2(
-                            label: Text('NAME', style: TextPalette.h3),
-                            size: ColumnSize.M),
-                        DataColumn2(
-                            size: ColumnSize.S,
-                            label: Text('SCORE', style: TextPalette.h3),
-                            tooltip: "Average score",
-                            numeric: true),
-                        DataColumn2(
-                            size: ColumnSize.S,
-                            label: Text('POTM', style: TextPalette.h3),
-                            numeric: true,
-                            tooltip: "Number of Player of the Match awards"),
-                        DataColumn2(
-                            size: ColumnSize.S,
-                            label: Text('MATCH', style: TextPalette.h3),
-                            numeric: true),
-                        DataColumn2(
-                            size: ColumnSize.S,
-                            label: Text('WIN %', style: TextPalette.h3),
-                            numeric: true,
-                            tooltip: "Percentage of games won"),
-                      ],
-                      rows: leaderboard!.entries
-                          .asMap()
-                          .entries
-                          .map((entry) => DataRow(cells: [
-                                DataCell(Align(
-                                  alignment: Alignment.centerRight,
-                                  child: Text(
-                                    (entry.key + 1).toString(),
-                                    style: TextPalette.getBodyText(
-                                        Palette.greyLight),
-                                  ),
-                                )),
-                                DataCell(Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: Row(
-                                      children: [
-                                        UserAvatar(10.0, context
-                                            .watch<UserState>()
-                                            .getUserDetail(
-                                            entry.value.userId)),
-                                        SizedBox(
-                                          width: 4,
-                                        ),
-                                        Expanded(
-                                          child: Align(
-                                            alignment: Alignment.centerLeft,
-                                            child: UserNameWidget(
-                                                userDetails: context
+                      height: MediaQuery.of(context).size.height,
+                      child: DataTable2(
+                          sortColumnIndex: sortColumnIndex,
+                          sortAscending: sortAscending,
+                          dataRowHeight: 36,
+                          headingRowHeight: 36,
+                          columnSpacing: 4,
+                          smRatio: 0.8,
+                          dividerThickness: 0,
+                          horizontalMargin: 0,
+                          onSelectAll: (val) => print('s'),
+                          columns: [
+                            DataColumn2(
+                              fixedWidth: 20,
+                              label: Text('', style: TextPalette.h3),
+                            ),
+                            DataColumn2(
+                                label: Text('NAME', style: TextPalette.h3),
+                                size: ColumnSize.M),
+                            DataColumn2(
+                                size: ColumnSize.S,
+                                label: Text('SCORE', style: TextPalette.h3),
+                                tooltip: "Average score",
+                                numeric: true),
+                            DataColumn2(
+                                size: ColumnSize.S,
+                                label: Text('POTM', style: TextPalette.h3),
+                                numeric: true,
+                                tooltip:
+                                    "Number of Player of the Match awards"),
+                            DataColumn2(
+                                size: ColumnSize.S,
+                                label: Text('MATCH', style: TextPalette.h3),
+                                numeric: true),
+                            DataColumn2(
+                                size: ColumnSize.S,
+                                label: Text('WIN %', style: TextPalette.h3),
+                                numeric: true,
+                                tooltip: "Percentage of games won"),
+                          ],
+                          rows: leaderboard!.entries
+                              .asMap()
+                              .entries
+                              .map((entry) => DataRow(cells: [
+                                    DataCell(Align(
+                                      alignment: Alignment.centerRight,
+                                      child: Text(
+                                        (entry.key + 1).toString(),
+                                        style: TextPalette.getBodyText(
+                                            Palette.greyLight),
+                                      ),
+                                    )),
+                                    DataCell(Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Row(
+                                          children: [
+                                            UserAvatar(
+                                                10.0,
+                                                context
                                                     .watch<UserState>()
                                                     .getUserDetail(
                                                         entry.value.userId)),
-                                          ),
-                                        )
-                                      ],
-                                    ))),
-                                DataCell(Text(
-                                    entry.value.averageScore
-                                            ?.toStringAsFixed(2) ??
-                                        "-",
-                                    style: TextPalette.bodyText)),
-                                DataCell(Text(entry.value.potmCount.toString(),
-                                    style: TextPalette.bodyText)),
-                                DataCell(Text(
-                                    entry.value.numMatchesJoined.toString(),
-                                    style: TextPalette.bodyText)),
-                                DataCell(Text(
-                                    entry.value
-                                            .getWinLossRatio()
-                                            ?.toStringAsFixed(0) ??
-                                        "-",
-                                    style: TextPalette.bodyText)),
-                              ]))
-                          .toList()),
-                )),
+                                            SizedBox(
+                                              width: 4,
+                                            ),
+                                            Expanded(
+                                              child: Align(
+                                                alignment: Alignment.centerLeft,
+                                                child: UserNameWidget(
+                                                    userDetails: context
+                                                        .watch<UserState>()
+                                                        .getUserDetail(entry
+                                                            .value.userId)),
+                                              ),
+                                            )
+                                          ],
+                                        ))),
+                                    DataCell(Text(
+                                        entry.value.averageScore
+                                                ?.toStringAsFixed(2) ??
+                                            "-",
+                                        style: TextPalette.getBodyText(
+                                            Palette.black))),
+                                    DataCell(Text(
+                                        entry.value.potmCount.toString(),
+                                        style: TextPalette.getBodyText(
+                                            Palette.black))),
+                                    DataCell(Text(
+                                        entry.value.numMatchesJoined.toString(),
+                                        style: TextPalette.getBodyText(
+                                            Palette.black))),
+                                    DataCell(Text(
+                                        entry.value
+                                                .getWinLossRatio()
+                                                ?.toStringAsFixed(0) ??
+                                            "-",
+                                        style: TextPalette.getBodyText(
+                                            Palette.black))),
+                                  ]))
+                              .toList()),
+                    )),
               if (leaderboard == null)
                 Padding(
                   padding: EdgeInsets.only(top: 36),
