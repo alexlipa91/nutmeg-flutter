1db32175b169dfb01408d62c008f69630592812e
Alessandro <alessandroliparoti@live.it>
2025-04-20 11:31:09 +0200

    don't use fvm in ci

--- scripts/build_web_app.sh
+++ scripts/build_web_app.sh
@@ -1,16 +1,23 @@
 # Set environment, default to prod if not specified
 ENV=${1:-prod}
 
+# Set the Flutter command based on environment
+FLUTTER_CMD="flutter"
+if [ -z "$GITHUB_ACTIONS" ]; then
+    echo "Running locally"
+    FLUTTER_CMD="fvm flutter"
+fi
+
 if [ "$ENV" = "staging" ]; then
     echo "Building for staging"
-    fvm flutter build web \
+    $FLUTTER_CMD build web \
         -t lib/screens/Launch.dart \
         --dart-define-from-file=.env.prod \
         --dart-define=BUILD_TIMESTAMP=$(date "+%Y%m%d-%H%M%S") \
         --release    
 else
     echo "Building for prod"
-    fvm flutter build web \
+    $FLUTTER_CMD build web \
         -t lib/screens/Launch.dart \
         --dart-define-from-file=.env.prod \
         --release    
