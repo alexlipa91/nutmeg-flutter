f9ec915fa869225a21e0bfceb432977ebcd5e93d
Alessandro <alessandroliparoti@live.it>
2025-05-03 19:11:49 +0200

    pass envs to build

--- scripts/build_android_app.sh
+++ scripts/build_android_app.sh
@@ -1,1 +1,6 @@
-flutter build appbundle --release
+# Read environment variables if set, otherwise use defaults
+ENV_FILE=${ENV_FILE:-".env.prod"}
+
+echo "Running build with env file: $ENV_FILE"
+
+flutter build appbundle --release --dart-define-from-file=$ENV_FILE
