bf0afee024666c058a717aba2fd48c75fdecf6bb
Alessandro <alessandroliparoti@live.it>
2026-02-09 11:21:09 +0100

    add missing secrets

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,8 @@
 from src import _create_app
 from src.common.cloud_logging import CloudLoggingHandler
-from dotenv import load_dotenv
 import firebase_admin
 import os
 
-
-load_dotenv(".env.local")
 
 def create_app():
     firebase_admin.initialize_app()
@@ -24,7 +21,6 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    load_dotenv(".env.local")
     # Used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
